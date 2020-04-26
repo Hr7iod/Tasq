2454961c4cfce0e104fbc128bb554706c55d7302
Hr7iod <hr7iod@gmail.com>
2020-04-27 00:58:09 +0400

    Проделаны все первоначальные настройки и добавлено логирование.

---
+++ Tasq/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LoggerService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Tasq.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private ILoggerManager _logger;
+
+        public WeatherForecastController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Инфо мессадж.");
+            _logger.LogDebug("Дебаг мессадж.");
+            _logger.LogWarn("Варн мессадж.");
+            _logger.LogError("Еррор мессадж.");
+
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
