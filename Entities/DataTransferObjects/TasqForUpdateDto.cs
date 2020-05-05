c3e28c5caafc7d54eaa3eae72f9599d92d7fdb32
Hr7iod <hr7iod@gmail.com>
2020-05-05 13:07:55 +0400

    Поддержка put-запросов

---
+++ Entities/DataTransferObjects/TasqForUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DataTransferObjects
+{
+    public class TasqForUpdateDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ParentId { get; set; }
+
+        public IEnumerable<TasqForCreationDto> Children { get; set; }
+    }
+}
